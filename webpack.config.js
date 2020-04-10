897a7a7c312087f51256fa68b6afa67bf05b1272
Vivek Patil <vipati@microsoft.com>
2020-04-10 10:07:23 -0700

    Adding the production mode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: "production",
     entry: './node_modules/es6-promise/dist/es6-promise.auto.js',
     output: {
         filename: 'main.js',
